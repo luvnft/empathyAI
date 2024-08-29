0a7c64e9057e8c29c0c2388ed3b35b56c5ad1789
jai <kjcreddy666@gmail.com>
2024-08-29 17:14:28 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import EmpathyAI from './components/empathyAI';
+import EmpathyAI from './components/EmpathyAI';
 import './App.css'
 
 export default function App() {
