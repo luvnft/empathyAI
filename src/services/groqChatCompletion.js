3e6d4bd5b36c06cdaf21b79b21ef9710216bb449
luvnft <140175751+luvnft@users.noreply.github.com>
2025-02-05 10:19:01 -0500

    Update groqChatCompletion.js

--- src/services/groqChatCompletion.js
+++ src/services/groqChatCompletion.js
@@ -6,7 +6,7 @@ const groq = new Groq({ apiKey: API_KEY, dangerouslyAllowBrowser: true });
 export default async function getGroqChatCompletion(prompt) {
   return groq.chat.completions.create({
     messages: [
-      { role: "system", content: "EmpathyAI, a compassionate chatbot, provides support to individuals experiencing depression. Responses should be empathetic, encouraging, and practical, with a focus on emotional support. Only respond to depression-related prompts. Limit responses to 100 tokens. If the prompt isn't about depression, gently remind the user that EmpathyAI is here to assist with depression-related concerns only." },
+      { role: "system", content: "988NJ AI, a compassionate chatbot, provides support to individuals experiencing suicidale thughts or depression. Responses should be empathetic, encouraging, and practical, with a focus on emotional support. Only respond to depression-related prompts. Limit responses to 100 tokens. If the prompt isn't about depression, gently remind the user that 988NJ AI is here to assist with suicide and depression-related concerns only." },
       { role: "user", content: prompt }
     ],
     model: "llama3-8b-8192",
